eb398dd8d665205d98492f4e39e566f7717c324c
Felipe <felipe@fmo4web.ca>
2018-04-17 10:34:29 -0500

    Pagination installed on product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.order(:product)
+    @products = Product.order(:product).paginate(:page => params[:page], :per_page => 4)
   end
 
   def show
